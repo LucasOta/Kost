5886c97a786cc6288512725f1f4608874b06b0b6
MarcosBlua <marcos.blua@users.noreply.github.com>
2018-10-27 10:51:19 -0300

    Capa negocio completa

---
+++ Kost/CapaNegocio/Categoria.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class Categoria
+    {
+        private bool error;
+        private string mensaje;
+        private string nombre;
+        public Categoria() { }
+
+        public Categoria(string categoria)
+        {
+            error = false;
+            mensaje = "";
+            if (!error)
+            {
+                this.Validar(categoria);
+                if (!error)
+                {
+                    this.Guardar(categoria);
+                }
+                else
+                {
+                    error = true;
+                }
+            }
+            else
+            {
+                error = true;
+            }
+        }
+
+        public bool Error
+        {
+            get
+            {
+                return error;
+            }
+
+            set
+            {
+                error = value;
+            }
+        }
+
+        public string Mensaje
+        {
+            get
+            {
+                return mensaje;
+            }
+
+            set
+            {
+                mensaje = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        //Funciones
+
+        private void Validar(string categ)
+        {
+            if (Validaciones.Categoria(categ))
+            {
+                this.error = true;
+                this.mensaje = "La categoria ya existe";
+            }
+        }
+
+        public void Guardar(string cnombre)
+        {
+            if (!error)
+            {
+                if (CapaDatos.CategoriaBD.guardar(cnombre))
+                {
+                    this.error = false;
+                    this.mensaje = "Categoría guardada";
+                }
+                else
+                {
+                    this.error = true;
+                    this.mensaje += "Hubo un Error con la BD, intente nuevamente";
+                }
+            }
+        }
+
+        public static Boolean Eliminar(int id)
+        {
+            return CapaDatos.CategoriaBD.eliminar(id);
+        }
+
+        public static Boolean ModificarCateg(int id, string nombre)
+        {
+            if (CapaDatos.CategoriaBD.modificar(id, nombre))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static DataTable ListarTodos()
+        {
+            return CapaDatos.CategoriaBD.Get_all();
+        }
+
+        public static Categoria TraerUnaCat(int id)
+        {
+            DataTable categ = CapaDatos.CategoriaBD.TraerUnaCategoria(id);
+
+            DataRow rowus = categ.Rows[0];
+
+            Categoria c = new Categoria();
+            return c;
+        }
+    }
+}
---
+++ Kost/CapaNegocio/Detalle.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class Detalle
+    {
+        private int nroComanda;
+        private int nroDetalle;
+        private int codProducto;
+        private string descripProducto;
+        private int cantidad;
+        private float precioUnitario;
+
+        private bool error;
+        private string mensaje;
+
+
+
+        //Getters y Setters
+        public int NroComanda
+        {
+            get
+            {
+                return nroComanda;
+            }
+
+            set
+            {
+                nroComanda = value;
+            }
+        }
+
+        public int NroDetalle
+        {
+            get
+            {
+                return nroDetalle;
+            }
+
+            set
+            {
+                nroDetalle = value;
+            }
+        }
+
+        public int CodProducto
+        {
+            get
+            {
+                return codProducto;
+            }
+
+            set
+            {
+                codProducto = value;
+            }
+        }
+
+        public string DescripProducto
+        {
+            get
+            {
+                return descripProducto;
+            }
+
+            set
+            {
+                descripProducto = value;
+            }
+        }
+
+        public int Cantidad
+        {
+            get
+            {
+                return cantidad;
+            }
+
+            set
+            {
+                cantidad = value;
+            }
+        }
+
+        public float PrecioUnitario
+        {
+            get
+            {
+                return precioUnitario;
+            }
+
+            set
+            {
+                precioUnitario = value;
+            }
+        }
+
+        public bool Error
+        {
+            get
+            {
+                return error;
+            }
+
+            set
+            {
+                error = value;
+            }
+        }
+
+        public string Mensaje
+        {
+            get
+            {
+                return mensaje;
+            }
+
+            set
+            {
+                mensaje = value;
+            }
+        }
+
+        //Constructores
+        public Detalle()
+        {
+
+        }
+
+        public Detalle(int nroC, int codProd, string descProd, int cant, float precioUni)
+        {
+            Error = false;
+            Mensaje = "";
+            this.Validar(cant);
+            if (!Error)
+            {
+                NroComanda = nroC;
+                CodProducto = codProd;
+                DescripProducto = descProd;
+                Cantidad = cant;
+                PrecioUnitario = precioUni;
+
+                this.Guardar(NroComanda, CodProducto, DescripProducto, Cantidad, PrecioUnitario);
+            }
+            else
+            {
+                Error = true;
+            }
+        }
+
+
+        //Funciones
+        protected void Validar(int cantidad)
+        {
+            //Aca hay que validar que la cantidad ingresada sea coherente con la cantidad de stock que queda el producto
+        }
+
+        protected void Guardar(int nroCom, int codProd, string descProd, int cant, float precioUni)
+        {
+
+        }
+
+        public static Boolean Modificar(int codProd, int cantidad, float precioUni, string descrip)
+        {
+            return CapaDatos.DetalleBD.modificar(codProd, cantidad, precioUni, descrip);
+        }
+
+        public static Boolean Eliminar(int nroDetalle)
+        {
+            return CapaDatos.DetalleBD.eliminar(nroDetalle);
+        }
+
+        public static DataTable TraerTodosDetalles(int nroComanda)
+        {
+            return CapaDatos.DetalleBD.TraerTodosDetalles(nroComanda);
+        }
+
+        public static Detalle TraerUnDetalle(int nroDetalle)
+        {
+            DataTable detal = CapaDatos.DetalleBD.TraerUnDetalle(nroDetalle);
+
+            DataRow rowus = detal.Rows[0];
+
+            Detalle det = new Detalle();
+
+            det.Cantidad = Convert.ToInt32(rowus["cantidad"].ToString());
+            det.CodProducto = Convert.ToInt32(rowus["codProd"].ToString());
+            det.DescripProducto = rowus["descripProd"].ToString();
+            det.NroComanda = Convert.ToInt32(rowus["nroComanda"].ToString());
+            det.NroDetalle = Convert.ToInt32(rowus["nroDetalle"].ToString());
+            det.PrecioUnitario = Convert.ToInt32(rowus["precioUni"].ToString());
+
+            return det;
+        }
+    }
+}
